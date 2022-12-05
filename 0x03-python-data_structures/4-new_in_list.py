24f78a032bd7596b9bc2d54e0cfe8f8bc0e7bc2e
Isaiah Terhid Barnabas <isaiahterhide@gmail.com>
2022-12-05 11:05:47 -0800

    4-main.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,5 +5,6 @@ def new_in_list(my_list, idx, element):
         return my_list
     elif idx > n:
         return my_list
-    my_list[idx] = element
-    return my_list
+    new =my_list[:]
+    new[idx] = element
+    return new
