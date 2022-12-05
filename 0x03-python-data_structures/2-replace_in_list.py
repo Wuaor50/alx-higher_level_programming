c44a11d5904f3b704d1baaa61163e25fba05633d
Isaiah Terhid Barnabas <isaiahterhide@gmail.com>
2022-12-05 06:00:57 -0800

    2-replace_in_list.py

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    n = len(my_list) - 1
+    if idx < 0:
+        return None
+    elif idx > n:
+        return None
+    my_list[idx] = element
+    return my_list[idx]
