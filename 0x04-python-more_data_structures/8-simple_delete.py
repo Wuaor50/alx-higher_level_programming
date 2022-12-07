582871e7ffca7a65ab2b4c8a9efe708c44facc2b
Isaiah Terhid Barnabas <isaiahterhide@gmail.com>
2022-12-07 14:48:54 -0800

    9-multiply_by_2.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if key != "":
-        del a_dictionary[key]
+    if key is not None:
+        if key in a_dictionary:
+            del a_dictionary[key]
     return a_dictionary
