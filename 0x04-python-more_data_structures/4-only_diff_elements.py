72bd1058e60cf7a571987a044f0a83278883ff27
Isaiah Terhid Barnabas <isaiahterhide@gmail.com>
2022-12-07 09:46:17 -0800

    4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    new = list(set_1) + list(set_2)
+    new = []
+    new = list(set_1 - set_2) + list(set_2 - set_1)
     return list(set(new))
