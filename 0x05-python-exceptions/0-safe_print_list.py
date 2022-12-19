d0c6548235bb56fee5dad01cc8f879cb21f1e97e
Isaiah Terhid Barnabas <isaiahterhide@gmail.com>
2022-12-19 15:42:10 -0800

    first commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    num = 0
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            num = num + 1
+        print()
+        return num
+    except Exception :
+        print()
+        return num
